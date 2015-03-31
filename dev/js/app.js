8a93384ae2aff8c5935d5f7c811862d0a9f0572c
Ulises Reyes <urjr8891@gmail.com>
2015-03-30 18:54:03 -0700

    added controller for background colors

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,13 @@
 var mySite = angular.module('mySite',['ngRoute','ngAnimate']);
 
+mySite.controller('ColorController',['$scope', function($scope){
+	$scope.colors = ['3fd47d','e74c3c','3498db','7f8c8d','f1c40f','9b59b6','34495e'];
+	$scope.getColor = function(){
+		return $scope.colors[Math.floor(Math.random()*$scope.colors.length)];
+	};
+	$scope.currentColor = $scope.getColor();
+}]);
+
 mySite.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 	when('/home',{
